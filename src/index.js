e150c63180d7149c1bd31b8d7ae1409c74a7985b
Animesh Kumar <kumaranimesh923@gmail.com>
2025-02-28 11:22:13 +0530

    fix: add initial tests and logic for tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+import { ModuleFilenameHelpers } from "webpack";
 import "./style.css";
+
+function Ship() {
+  let length = 0;
+  let timesHit = 0;
+  // const isShipSunk = false;
+  function setShipLength(size) {
+    length = size;
+  }
+
+  function hit() {
+    timesHit++;
+  }
+
+  function isSunk() {
+    if (timesHit >= length) {
+      return true;
+    }
+    return false;
+  }
+
+  return {
+    isSunk,
+    hit,
+    setShipLength,
+  };
+}
+
+module.exports = {
+  Ship,
+};
---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+const { Ship } = require("./index");
+
+test("ship is floating", () => {
+  const s1 = Ship();
+  s1.setShipLength(5);
+  s1.hit();
+  s1.hit();
+  s1.hit();
+  expect(s1.isSunk()).toBe(false);
+});
+
+test("ship is sunk", () => {
+  const s1 = Ship();
+  s1.setShipLength(5);
+  s1.hit();
+  s1.hit();
+  s1.hit();
+  s1.hit();
+  s1.hit();
+  expect(s1.isSunk()).toBe(true);
+});
